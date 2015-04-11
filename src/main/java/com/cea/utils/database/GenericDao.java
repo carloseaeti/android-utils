a7851a7921a21fe17015090e00d7ed160c91fd79
Carlos E Araujo <carlosaraujo@trixlog.com>
2015-04-11 09:19:50 -0300

    add RepositoryFactor

--- src/main/java/com/cea/utils/database/GenericDao.java
+++ src/main/java/com/cea/utils/database/GenericDao.java
@@ -1,5 +1,7 @@
 package com.cea.utils.database;
 
+import android.content.Context;
+
 import com.j256.ormlite.dao.Dao;
 import java.util.List;
 
@@ -8,35 +10,34 @@ import java.util.List;
  */
 public abstract class GenericDao {
 
-    public abstract DefaultRepository getRepository();
+    protected DefaultRepository repository;
+
+    public GenericDao(Context context){
+        repository = RepositoryFactory.getRepository(context);
+    }
 
     public void createOrUpdate(){
-        DefaultRepository repository = getRepository();
         repository.createOrUpdate(this);
     }
 
     public void delete(){
-        DefaultRepository repository = getRepository();
         repository.delete(this);
     }
 
     public <T> List<T> getAll(){
-        DefaultRepository repository = getRepository();
         return repository.getAll(getClass());
     }
 
     public <T> T findById(Long id){
-        DefaultRepository repository = getRepository();
         return repository.findEntityById(getClass(), id);
     }
 
     public long getCount() {
-        DefaultRepository repository = getRepository();
         return repository.getCount(getClass());
     }
 
     public <T> Dao<T, ?> getDao(){
-        Dao dao = getRepository().getDao(getClass());
+        Dao dao = repository.getDao(getClass());
         return dao;
     }
 }
---
+++ src/main/java/com/cea/utils/database/RepositoryFactory.java
@@ -0,0 +1,26 @@
+package com.cea.utils.database;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by carlos.araujo on 11/04/2015.
+ */
+public abstract class RepositoryFactory {
+
+    private static RepositoryFactory repositoryFactory;
+    public abstract DefaultRepository getRepository();
+
+    static DefaultRepository getRepository(Context context){
+        if(repositoryFactory == null) {
+            try {
+                ApplicationInfo app = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+                repositoryFactory = (RepositoryFactory) Class.forName(app.metaData.getString("com.cea.database.RepositoryFactory")).newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("RepositotyFactory not declared in Manifest. Use meta-data name 'com.cea.database.RepositoryFactory'");
+            }
+        }
+        return repositoryFactory.getRepository();
+    }
+}
