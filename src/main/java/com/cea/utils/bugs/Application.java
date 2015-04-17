a939fb58fa2ca465c5a6b2b8601498fcdf9d8988
Carlos E Araujo <carlosaraujo@trixlog.com>
2015-04-17 09:56:23 -0300

    Application refactor

--- src/main/java/com/cea/utils/bugs/Application.java
+++ src/main/java/com/cea/utils/bugs/Application.java
@@ -1,17 +1,26 @@
 package com.cea.utils.bugs;
 
+import android.content.Context;
+
 /**
  * Created by carlos.araujo on 08/01/2015.
  * Fix AsyncTask 'never ends' problem. See: https://code.google.com/p/android/issues/detail?id=20915
  */
 public class Application extends android.app.Application {
 
+    private static Context applicationContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        applicationContext = this;
         try {
             Class.forName("android.os.AsyncTask");
         } catch (ClassNotFoundException e) {
         }
+    }
+
+    public static Context getContext(){
+        return applicationContext;
     }
 }
